25ac25a1450ad8cf7385fc1be28b58c72d95938b
mohmedhayder <mohmedaiyaeldeen@gmail.com>
2022-01-02 22:25:28 +0000

    up

--- srcipt.sh
+++ srcipt.sh
@@ -1,5 +1,12 @@
-echo "stop odoo server"
-sudo service odoo-server stop
+echo "stopping  odoo server"
+sudo service boy-server stop
+echo "odoo server stoped"
+echo "restarting nginx server"
 sudo service nginx restart
-sudo pkill -f odoo-server -9
-sudo service odoo-server start
+echo "nginx restarted"
+echo "killing odoo proccess"
+sudo pkill -f boy-server -9
+echo "odoo processes kiled succesfully"
+echo "starting odoo server"
+sudo service boy-server start
+echo "odoo server started"
